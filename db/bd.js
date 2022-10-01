b843ddbefa9f5335d413eaafe05d961eb3e413c8
EdgarCalel <edgar_kll@hotmail.com>
2022-09-30 20:00:57 -0600

    edgar

--- db/bd.js
+++ db/bd.js
@@ -1,9 +1,19 @@
-const  {Sequelize}  = require('sequelize');
 require('dotenv').config();
+const  {Sequelize}  = require('sequelize');
 const {
   DATABASE_URL
 }= process.env;
 
-const sequelize = new Sequelize(`${DATABASE_URL}`)
+
+const sequelize = new Sequelize(`${DATABASE_URL}`,{
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions:{
+   ssl: {
+ require:true,
+   rejectUnauthorized:false
+  }                             
+   },
+});
 
 module.exports = {sequelize}
