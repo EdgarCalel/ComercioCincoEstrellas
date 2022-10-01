391cc28484e177630a0a5956c734006463074873
EdgarCalel <edgar_kll@hotmail.com>
2022-09-30 19:06:55 -0600

    edgar

--- routes/status.route.js
+++ routes/status.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 //importaciones
-const {status } = require('../controller/status.controller')
+const {status } = require('../controller//status.controller')
 
 //routes
 router.get('/', status);
